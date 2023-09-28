0cc179daa9f922b6604828667d701d9f8420f4d0
Harshit <harshit25sodhani@gmail.com>
2023-09-28 13:41:18 +0530

    build commit -2

--- backend/Routes/book.js
+++ backend/Routes/book.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {getBooks, createBook, updateBook, deleteBook, getBookById} = require("../Controller/Book.js")
+const {getBooks, createBook, updateBook, deleteBook, getBookById} = require("../Controller/book.js")
 
 router.get("/", getBooks);
 router.get("/:id", getBookById);
